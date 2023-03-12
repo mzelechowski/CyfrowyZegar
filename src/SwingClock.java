fda29a76093fb2d85809007fc8ba1e3f93b51995
maciek zelek <mzelechowski79@gmail.com>
2023-03-12 14:29:17 +0100

    one file project

---
+++ src/SwingClock.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.time.LocalDateTime;
+
+public class SwingClock extends JFrame implements Runnable {
+
+    private Thread thread;
+    String hour, minutes, seconds;
+    JLabel label;
+
+    public SwingClock() {
+        label = new JLabel("", JLabel.CENTER);
+        label.setBounds(5, 5, 100, 40);
+        add(label);
+        setSize(100, 80);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            thread.sleep(1000);
+            while (true) {
+                LocalDateTime today = LocalDateTime.now();
+                hour = "" + today.getHour();
+                minutes = String.format("%02d", today.getMinute());
+                seconds = String.format("%02d", today.getSecond());
+                label.setText(hour + ":" + minutes + ":" + seconds);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingClock clock = new SwingClock();
+    }
+
+
+}
